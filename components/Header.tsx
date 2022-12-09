4729d83c8afef8e847bccd328015aea7f2bf92bd
sofuetakuma <kawahagi0620@gmail.com>
2022-12-10 00:21:37 +0900

    Header修正

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,10 @@
 export default function Header() {
   return (
     <div class="bg-white w-full py-6 px-8">
-      <div class="text-2xl ml-1 font-bold text-center">Fresh</div>
+      <div class="text-2xl ml-1 font-bold text-center flex justify-center">
+        <span class="pr-[12px]">Sofue's Fresh App</span>
+        <img src="https://storage.googleapis.com/zenn-user-upload/topics/ac0db492fd.png" class="h-[30px]" />
+      </div>
     </div>
   );
 }
